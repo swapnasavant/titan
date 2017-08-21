19b19b77264368c6b64217e88eacf4c1294e7eb0
Swapna Savant <swapnasavant@gmail.com>
2017-08-21 08:14:09 -0700

    Slider

--- src/components/ImageCompare.js
+++ src/components/ImageCompare.js
@@ -52,22 +52,6 @@ class ImageCompare extends Component {
                   onChange={this.dragger}
                 />
              }
-             <div className="fff">
-               <svg width="100" height="100" viewBox="0 0 100 100">
-                 <g className="group" opacity="0.8">
-                   <g className="large">
-                     <path id="large" d="M41.25,40 L42.5,10 L43.75,40 L45, 41.25 L75,42.5 L45,43.75
-                       L43.75,45 L42.5,75 L41.25,45 L40,43.75 L10,42.5 L40,41.25z " fill="gold" />
-                   </g>
-                   <g className="large-2" transform="rotate(45)">
-                   </g>
-                   <g className="small">
-                     <path id="small" d="M41.25,40 L42.5,25 L43.75,40 L45,41.25 L60,42.5 L45,43.75
-                      L43.75,45 L42.5,60 L41.25,45 L40,43.75 L25,42.5 L40,41.25z" fill="gold" />
-                   </g>
-                 </g>
-               </svg>
-             </div>
             </div>
          }
           { vertical &&
